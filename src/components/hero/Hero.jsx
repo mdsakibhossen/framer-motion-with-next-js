860071ecc47777eb69dea9dfdc99ebe933a7f3dc
Md Sakib Hossen <sakib.hossen0143@gmail.com>
2024-10-02 16:56:05 +0600

    Button variants is Added

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,16 +2,28 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 
+const buttonVariants = {
+  hidden: {
+    y: "200px",
+    x: "-50%",
+  },
+  visible: {
+    y: 0,
+    x: "-50%",
+    transition: { delay: 0.75 },
+  },
+};
+
 const Hero = () => {
   const [isViewed, setIsViewed] = useState(false);
   const products = ["Apple", "Mango", "Orange", "Jackfruits"];
   return (
     <section className="py-20 h-screen flex justify-center items-center bg-slate-800 text-white relative overflow-hidden">
       <motion.button
+        variants={buttonVariants}
         whileHover={{ scale: 1.2, boxShadow: "0px 0px 10px 1px #3B82F6" }}
-        initial={{ y: "200px", x: "-50%" }}
-        animate={{ y: 0, x: "-50%", transition: { delay: 0.75 } }}
-        // transition={{ delay: 0.75 }}
+        initial="hidden"
+        animate="visible"
         onClick={() => setIsViewed((pv) => !pv)}
         className="px-10 py-2.5 rounded bg-blue-500 absolute bottom-10 left-1/2 -translate-x-1/2"
       >
@@ -21,7 +33,7 @@ const Hero = () => {
         <motion.div
           initial={{ x: "-100vw", opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
-          transition={{ type: "spring", stiffness: 300,damping: 15 }}
+          transition={{ type: "spring", stiffness: 300, damping: 15 }}
           className="content"
         >
           <h3 className="text-lg font-medium mb-3">My Products: </h3>
